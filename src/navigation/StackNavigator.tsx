8da28ffea568a42d8d7941adf08d6abed3f36f58
ot0h <rimv715@gmail.com>
2026-08-21 20:37:32 -0600

    Dejando por lo menos el layout pal navigation, agregando la depedencia de los iconos xd, y ajustando cosas del tsconfig

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { LoginScreen } from '@screens/LoginScreen'
+
+export type RootStackParamList = {
+  Login: undefined
+  MainTabs: { email: string }
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      {/* <Stack.Screen name="MainTabs" component={} /> */}
+    </Stack.Navigator>
+  )
+}
