15f296068a6457ea545ced56e8544113e3701f97
walterdiaz1993 <waltermisael1108@gmail.com>
2026-08-21 20:47:12 -0600

    Estructura de Carpetas

---
+++ src/screens/tabs/ProfileTab.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ProfileTab() {
+  return (
+    <View>
+      <Text>ProfileTab</Text>
+    </View>
+  );
+}
