bb51caab51503813ec34e2265ef56c6dd1fad928
KennethC01 <kennethcaballerol14@gmail.com>
2026-08-21 20:32:11 -0600

    arregle errores del TabNavigator.tsx  ya es funcional

--- MiAppLab/src/navigation/TabsNavigator.tsx
+++ MiAppLab/src/navigation/TabsNavigator.tsx
@@ -1,5 +1,49 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
+import { RootStackParamList } from './StackNavigator';
+import HomeTab from '../screens/tabs/HomeTab';
+import IMCTab from '../screens/tabs/IMCTab';
+import ProfileTab from '../screens/tabs/ProfileTab';
+
 export type TabsParamList = {
   Inicio: { email: string };
   IMC: undefined;
   Perfil: { email: string };
 };
+
+type Props = NativeStackScreenProps<RootStackParamList, 'MainTabs'>;
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function TabsNavigator({ route }: Props) {
+  const { email } = route.params;
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: '#5f0650',
+        tabBarInactiveTintColor: '#888888',
+        tabBarIcon: ({ color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'help-circle';
+
+          if (route.name === 'Inicio') {
+            iconName = 'home';
+          } else if (route.name === 'IMC') {
+            iconName = 'calculator';
+          } else if (route.name === 'Perfil') {
+            iconName = 'person';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Inicio" component={HomeTab} initialParams={{ email }} />
+      <Tab.Screen name="IMC" component={IMCTab} />
+      <Tab.Screen name="Perfil" component={ProfileTab} initialParams={{ email }} />
+    </Tab.Navigator>
+  );
+}
