427f8e357200a861b15eb35e96c53b0f591ca220
Stefany <stefany_madrid@unitec.edu>
2026-08-22 17:13:01 -0600

    -se agrego tabs navigator con tabs inicio, imc y perfil

---
+++ MiAppLab/src/navigation/TabsNavigator.tsx
@@ -0,0 +1,42 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
+import { RootStackParamList } from './StackNavigator';
+import HomeTab from '../screens/tabs/HomeTab';
+import IMCTab from '../screens/tabs/IMCTab';
+import ProfileTab from '../screens/tabs/ProfileTab';
+
+export type TabsParamList = {
+  Inicio: { email: string };
+  IMC: undefined;
+  Perfil: { email: string };
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'MainTabs'>;
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function TabsNavigator({ route }: Props) {
+  const { email } = route.params;
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: '#5f0650',
+        tabBarInactiveTintColor: '#999',
+        tabBarIcon: ({ color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'home';
+          if (route.name === 'Inicio') iconName = 'home';
+          else if (route.name === 'IMC') iconName = 'fitness';
+          else if (route.name === 'Perfil') iconName = 'person';
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Inicio" component={HomeTab} initialParams={{ email }} />
+      <Tab.Screen name="IMC" component={IMCTab} />
+      <Tab.Screen name="Perfil" component={ProfileTab} initialParams={{ email }} />
+    </Tab.Navigator>
+  );
+}
