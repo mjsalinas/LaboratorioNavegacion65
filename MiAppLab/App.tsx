14395862f4e4d35ce19f3598acc861bc20ff09c8
davidxdiaz <02davidxdiaz@gmail.com>
2026-08-22 18:29:33 -0600

    Configuración inicial

--- MiAppLab/App.tsx
+++ MiAppLab/App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import StackNavigator from './src/navigation/StackNavigator'; 
+import { navigationRef } from './src/navigation/NavigationService'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default function App() { 
+  return ( 
+    <NavigationContainer ref={navigationRef}> 
+      <StackNavigator /> 
+    </NavigationContainer> 
+  ); 
+} 
 
 const styles = StyleSheet.create({
   container: {
