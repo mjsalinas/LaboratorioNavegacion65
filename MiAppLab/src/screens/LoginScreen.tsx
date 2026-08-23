16dea53219901173115477372cad95563205746c
DoubleGG15 <giannimelgar3@gmail.com>
2026-08-22 22:32:21 -0600

    feat: LoginScreen
    Se pasa el email por ruta y se implementan Custombuttons

---
+++ MiAppLab/src/screens/LoginScreen.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/StackNavigator';
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+export default function LoginScreen({ navigation }: Props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (email && password.length >= 4) {
+      navigation.navigate('MainTabs', { email });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <CustomInput
+        type='email'
+        placeholder='Correo electrónico'
+        value={email}
+        onChange={setEmail}
+      />
+      <CustomInput
+        type='password'
+        placeholder='Contraseña'
+        value={password}
+        onChange={setPassword}
+      />
+      <CustomButton
+        title='Iniciar sesión'
+        onPress={handleLogin}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 24, justifyContent: 'center' },
+  title: { fontSize: 28, fontWeight: 'bold', marginBottom: 24, textAlign: 'center', color: '#5f0650' },
+});
