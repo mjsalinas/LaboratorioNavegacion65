af8e71de52cc932c67e76c7767cadb65226c3de0
heydI1-glitch <heydimolina019@gmail.com>
2026-08-22 20:46:00 -0600

    Actualización y cambios en App.tsx y package.json

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-// App.tsx
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StackNavigator from './src/Navigation/StackNavigator';
 import { navigationRef } from './src/Navigation/NavigationService';
-
 export default function App() {
   return (
     <NavigationContainer ref={navigationRef}>
